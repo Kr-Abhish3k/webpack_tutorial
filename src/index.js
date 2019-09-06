f9011c45528dabb0ef98d306da6d6c1877f95c93
kumarAbhishek138 <abhishek.kumarsudhir@springernature.com>
2019-09-06 15:16:18 +0530

    Webpack now bundling all our app code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log("hello webpack");
+import { run } from "./app/app.js";
+import { AlertService } from "./app/alert.service";
+import { ComponentService } from "./app/component.service";
+
+const alertService = new AlertService();
+const componentService = new ComponentService();
+run(alertService, componentService);
