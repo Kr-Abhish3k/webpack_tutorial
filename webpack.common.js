169c19226862a6edf261fc2deefd9fa2275892e7
kumarAbhishek138 <abhishek.kumarsudhir@springernature.com>
2019-09-06 19:25:15 +0530

    Add html-loader, file-loader, and clean-webpack-plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,6 @@
 var htmlWebpackPlugin = require("html-webpack-plugin");
+const { cleanWebpackPlugin } = require("clean-webpack-plugin");
+
 const path = require("path");
 module.exports = {
 	entry: "./src/index.js",
@@ -15,6 +17,20 @@ module.exports = {
 					"css-loader", //2.translates css to js using css loader      ™  ^
 					"sass-loader" // 1.convert sass to css using sass-loader       ^
 				]
+			},
+			{
+				test: /\.html$/,
+				use: ["html-loader"]
+			},
+			{
+				test: /\.(svg|png|jpeg|jpg|gif)$/,
+				use: {
+					loader: "file-loader",
+					options: {
+						name: "[name].[hash].[ext]",
+						outputPath: "imgs"
+					}
+				}
 			}
 		]
 	},
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,8 @@
 const common = require("./webpack.common");
 const merge = require("webpack-merge");
 const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
 module.exports = merge(common, {
 	mode: "production", //[will not minify the output file]
 	//devtool: "none",
@@ -8,5 +10,6 @@ module.exports = merge(common, {
 	output: {
 		filename: "main.[contentHash].js",
 		path: path.resolve(__dirname, "scripts")
-	}
+	},
+	plugins: [new CleanWebpackPlugin()]
 });
